f785c6abeef3ec31951ffb3ea85602b907e98dd0
zaleskilucas <zaleski.lucas@escola.pr.gov.br>
2025-10-03 07:43:29 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+print("*** Mini Questionário ***\n")
+nome = input("Qual seu nome?\n")
+idade = input("Qual a sua idade?\n")
+genero = input("Você é homem ou mulher?\n")
+endereco = input("Qual o seu endereço?\n")
+
+print("Olá, ", nome, "\n")
+print("\n")
